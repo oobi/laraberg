1ff44485065d1bfd2d2d25feab2da82cc3a81e0e
Chris Carey <503084+oobi@users.noreply.github.com>
2026-02-17 15:42:20 +1100

    block parser

--- src/Blocks/Wordpress/WordPressBlockParserBlock.php
+++ src/Blocks/Wordpress/WordPressBlockParserBlock.php
@@ -78,9 +78,9 @@ class WordPressBlockParserBlock
      * @param string $innerHTML    Resultant HTML from inside block comment delimiters after removing inner blocks.
      * @param array  $innerContent List of string fragments and null markers where inner blocks were found.
      */
-    public function __construct(string $name, array $attrs, array $innerBlocks, string $innerHTML, array $innerContent)
+    public function __construct(?string $name, array $attrs, array $innerBlocks, string $innerHTML, array $innerContent)
     {
-        $this->blockName    = $name;
+        $this->blockName    = $name ?? '';
         $this->attrs        = $attrs;
         $this->innerBlocks  = $innerBlocks;
         $this->innerHTML    = $innerHTML;
